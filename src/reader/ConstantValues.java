7a0c9b5ce324d5fc2b1243470934ba915a10c0c1
Nikolay Pelov <nikolai_pelov@abv.bg>
2017-05-15 11:54:06 +0200

    Fixed overwrite

--- src/reader/ConstantValues.java
+++ src/reader/ConstantValues.java
@@ -6,6 +6,7 @@ public class ConstantValues {
     public static final String PLAY_BUTTON_IMAGE_FILE = "images/play.png";
     public static final String PAUSE_BUTTON_IMAGE_FILE = "images/pause.png";
     public static final String MAP_SHAPE_FILE = "maps/ne_110m_admin_0_countries.shp";
-    public static final int POINT_RADIUS = 3;
+    public static final int POINT_RADIUS = 6;
+
 
 }
