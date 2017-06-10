1395dcdd97a694883745688823bf54ea3c00feb1
Slavchev,Veselin V.R <v.slavchev@student.fontys.nl>
2017-06-10 18:42:08 +0200

    Finish cities crawler

--- scripts/cities-crawler.py
+++ scripts/cities-crawler.py
@@ -3,40 +3,47 @@ import re
 from lxml import html
 import unicodedata
 
-# textFile = open('countriesAverageTemperatures.txt', 'w')
+def cleanPopulation(text):
+    return ''.join(list(filter(lambda ch: ch not in [",", "."], text)))
+
+textFile = open('cities.txt', 'w')
 
 countriesListPage = requests.get('http://data.mongabay.com/igapo/largest_cities.htm')
 countriesListTree = html.fromstring(countriesListPage.content)
 
 # 'a' tags of all the countries
 countryEntries = countriesListTree.xpath('//tr/td/font/a')
-for country in countryEntries[:2]:
+for country in countryEntries:
 
     countryName = country.xpath('.//text()')[0]
-    print(countryName)
+    dataLine = "country#" + countryName + '\n'
+    textFile.write(dataLine)
+    print(dataLine)
+
     citiesListPage = requests.get(country.xpath('.//@href')[0])
     citiesListTree = html.fromstring(citiesListPage.content)
 
     cityRows = citiesListTree.xpath('//table[@id="myTable"]//tr')
-    # 20% of len(cityRows) then min it with 3?
-    for city in cityRows:
+    # 20% of len(cityRows) or at least 5
+    for city in cityRows[:max(int(len(cityRows)/5), 5)]:
         cityPage = requests.get(city.xpath('.//td/a/@href')[0])
         cityTree = html.fromstring(cityPage.content)
 
         cityName = city.xpath('.//td/a/text()')[0]
         cityPopulation = city.xpath('.//td[2]/text()')[0]
-        cityLocationLink = cityTree.xpath('//a[contains(text(), "Google Earth")]/@href')[0]
+        cityLocationLink = ""
+        try:
+            cityLocationLink = cityTree.xpath('//a[contains(text(), "Google Earth")]/@href')[0]
+        except IndexError:
+            continue
 
         # to extract the coordinates
-        regexCaptureCoordinates = re.compile(".*&ll=([\d.,]+)&.*")
-        cityCoordinates = regexCaptureCoordinates.match(cityLocationLink).group(1)
+        regexCaptureCoordinates = re.compile(".*&ll=([\d.-]+),([\d.-]+)&.*")
+        cityLatitude = regexCaptureCoordinates.match(cityLocationLink).group(1)
+        cityLongitude = regexCaptureCoordinates.match(cityLocationLink).group(2)
 
-        print("    " + cityName + " " + cityPopulation + " " + cityCoordinates)
+        dataLine = "city#" + cityName + "#" + cleanPopulation(cityPopulation) + "#" + cityLatitude + "#" + cityLongitude + '\n'
+        textFile.write(dataLine)
+        print(dataLine)
 
-
-
-
-    # print(str(dataLine) + ' # Number of items: ' + str(len(dataLine)))
-    # textFile.write(str(dataLine) + '\n')
-
-# textFile.close()
+textFile.close()
